4b09ec670049904a24e7126a934276a943d2287f
leongdev <leongdev@gmail.com>
2023-10-19 15:31:15 -0700

    Setting project

---
+++ cashfocus/cashfocus/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  cashfocus
+//
+//  Created by Leon Walras Moura da Silva  on 2023-10-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+} 
