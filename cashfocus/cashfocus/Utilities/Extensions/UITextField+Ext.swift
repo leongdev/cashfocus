5b399feda9a36bda43f0ca09852b2215065ea491
leongdev <leongdev@gmail.com>
2023-10-29 12:52:25 -0700

    Improving UX of the keyboard

---
+++ cashfocus/cashfocus/Utilities/Extensions/UITextField+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  UITextField+Ext.swift
+//  cashfocus
+//
+//  Created by Leon Walras Moura da Silva  on 2023-10-29.
+//
+
+import Foundation
---
+++ cashfocus/cashfocus/Utilities/Extensions/UITextView+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  UITextView+Ext.swift
+//  cashfocus
+//
+//  Created by Leon Walras Moura da Silva  on 2023-10-29.
+//
+
+import Foundation
